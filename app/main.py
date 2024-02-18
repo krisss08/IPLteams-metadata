5eb827707949809b8583991e8def15966ce3b9c1
krisss08 <krishi@madstreetden.com>
2024-02-18 21:57:39 +0530

    response model

--- app/main.py
+++ app/main.py
@@ -13,10 +13,22 @@ class TeamMetaData(BaseModel):
     team_id: Optional[str] = None
     team_name: str
     city: str
-    owner: str
-    coach: str
+    owner: Optional[str]
+    coach: Optional[str]
     captain: str
-    established_year: int
+    established_year: Optional[int]
+    year_won: Optional[List[int]]
+
+
+class TeamMetaDataResponse(BaseModel):
+    team_id: Optional[str] = None
+    team_name: str
+    city: str
+    owner: Optional[str]
+    coach: Optional[str]
+    captain: str
+    established_year: Optional[int]
+    year_won: Optional[str]
 
 class TeamExistsResponse(BaseModel):
     team_id: str
